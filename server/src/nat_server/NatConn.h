873913b53a3d2faf99acbc8df7e857a0368ae545
xtong <xieqq8@163.com>
2016-05-16 10:19:29 +0800

    xie

--- server/src/nat_server/NatConn.h
+++ server/src/nat_server/NatConn.h
@@ -8,8 +8,7 @@
 #ifndef __NATCONN_H__
 #define __NATCONN_H__
 
-#include "netlib.h"
-#include "util.h"
+#include "imconn.h"
 
 class CNatConn : public CImConn
 {
@@ -21,28 +20,11 @@ public:
 
 	virtual void OnConnect(net_handle_t handle);
 	virtual void OnClose();
-	virtual void OnTimer(uint64_t curr_tick);
 
 	virtual void HandlePdu(CImPdu* pPdu);
-
-private:
-	void _HandleOnlineUserInfo(CImPdu* pPdu);
-	void _HandleUserStatusUpdate(CImPdu* pPdu);
-	void _HandleRoleSet(CImPdu* pPdu);
-	void _HandleUsersStatusRequest(CImPdu* pPdu);
-	void _HandleMsgReadNotify(CImPdu* pPdu);
-	void _HandleKickUser(CImPdu* pPdu);
-    
-	void _DispatchFriend(uint32_t friend_cnt, uint32_t* friend_id_list);
-
-	void _BroadcastMsg(CImPdu* pPdu, CNatConn* pFromConn = NULL);
-    
-private:
-    void _UpdateUserStatus(uint32_t user_id, uint32_t status, uint32_t client_type);
-    void _SendPduToUser(uint32_t user_id, CImPdu* pPdu, bool bAll);
-    
-private:
-	bool			m_bMaster;
+private:	
+	void OnUDPRead();
+	void OnUDPWrite();
 
 protected:
 	net_handle_t	m_sock_handle;
@@ -51,9 +33,9 @@ protected:
 
 typedef hash_map<uint32_t, CNatConn*> NatConnMap_t;
 
-CNatConn* FindHttpConnByHandle(uint32_t handle);
+CNatConn* FindNatConnByHandle(uint32_t handle);
 
-
+/*
 // Client登录时向服务器发送的消息
 struct stLoginMessage
 {
@@ -84,5 +66,5 @@ struct stMessage
 		stP2PTranslate translatemessage;// 请求打洞
 	}message;
 };
-
+*/
 #endif /* NATCONN_H_ */
